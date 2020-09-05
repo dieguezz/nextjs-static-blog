bc3c69dc86bd8049c9d31ab94f1796234fe63f7b
dieguin <dieguezz@gmail.com>
2020-09-05 15:09:56 +0200

    :tada: rebuild path

--- docs/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/article/[id]":["static\u002Fchunks\u002Fpages\u002Farticle\u002F[id].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
